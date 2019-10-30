2c937cbef3dfb7cbe736408c1bdd6bfb0dd12e2d
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-10-29 18:30:12 -0700

    Add Grid and Paper to styling. Make responsive.

--- src/components/Followers.js
+++ src/components/Followers.js
@@ -1,29 +1,48 @@
 import React from 'react';
-// import { makeStyles, Card, CardMedia } from '@material-ui/core';
+import { makeStyles, Paper, Grid, Card, CardMedia } from '@material-ui/core';
 
 
-// const useStyles = makeStyles(theme => ({
-//     card: {
-//       maxWidth: 345,
-//     },
-//     media: {
-//         height: 0,
-//         paddingTop: '56.25%', // 16:9
-//       },
-// }));
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+      },
+      paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+      },
+    card: {
+      maxWidth: 345,
+    },
+    media: {
+        height: 0,
+        paddingTop: '100%', // 16:9
+    
+      },
+}));
 
 function Followers(props) {
-    // const classes = useStyles();
+    const classes = useStyles();
     console.log(props)
         return (
-            <div>
-                {props.data.map((peeps, id) => (
-                    <div>
-                        <img src={peeps.avatar_url} alt="" />
-                        <h1>{peeps.login}</h1>
-                    </div>
-                ))
-            }
+            <div className={classes.root}>
+                {/* <Grid container spacing={3}> */}
+                    <Grid item xs={12} sm={6}>
+                        <Paper className={classes.paper}>
+                            {props.data.map((peeps, id) => (
+                                <Card className={classes.card}>
+                                    <CardMedia
+                                    className={classes.media}
+                                    image={peeps.avatar_url}
+                                    title={peeps.login}
+                                    />
+                                    <h1>{peeps.login}</h1>
+                                </Card>
+                                ))
+                            }
+                        </Paper>
+                    </Grid>
+                {/* </Grid> */}
             </div>
         )
 }
